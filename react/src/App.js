8008775dde9a9f2fa4a57b308cc9ab468921d6a1
Ryan Mei <rymei@sfu.ca>
2020-08-02 15:57:36 -0700

    Login functionality with redirect if user not authenticated

--- react/src/App.js
+++ react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderBar from './components/HeaderBar';
-import {Redirect, Route} from "react-router";
+import {Redirect, Route, useHistory} from "react-router";
 import {BrowserRouter} from "react-router-dom";
 import Main from "./pages/Main";
 import Restaurant from "./pages/Restaurant";
@@ -11,31 +11,58 @@ import UserService from "./services/user.service"
 import Login from "./pages/Login"
 import Register from "./pages/Register";
 import OrderHistory from "./pages/OrderHistory";
-
-function App() {
-    // Test function to always return true, need to change when login implementation is complete
-    const isLoggedIn = UserService.getTestBoard();
-
-    return (
-        <div className="App" align="center">
-            <HeaderBar/>
-            <SearchBar/>
-            <BrowserRouter>
-                <Route exact path="/" component={Main}/>
-                <Route path="/restaurants/:restaurant" component={Restaurant}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <Route exact path="/orderhistory" component={OrderHistory}/>
-                {isLoggedIn ? (
-                    <div></div>
-                ) : (
-                    <Redirect to="/login"/>
-                )}
-            </BrowserRouter>
+import AuthService from "./services/auth.service"
+import Spinner from "react-bootstrap/Spinner";
 
 
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
+        if (user) {
+            console.log(user);
+            this.setState({
+                isLoggedIn: true,
+                isLoaded: true
+            });
+        }
+        else {
+            this.setState({
+                isLoggedIn: false,
+                isLoaded: true
+            });
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="App" align="center">
+                <HeaderBar/>
+                <SearchBar/>
+                <BrowserRouter>
+                    <Route exact path="/" component={Main}/>
+                    <Route path="/restaurants/:restaurant" component={Restaurant}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/orderhistory" component={OrderHistory}/>
+                    {!this.state.isLoggedIn && this.state.isLoaded ? (
+                        <Redirect to="/login"/>
+                    ) : (
+                        <div></div>
+                    )}
+                </BrowserRouter>
+            </div>
+
+        );
+    }
 }
 
 export default App;

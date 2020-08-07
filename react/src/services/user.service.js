eb4e5f376a74115ead50e1cc1f91f02cca5d3aea
Ryan Mei <rymei@sfu.ca>
2020-08-06 18:26:19 -0700

    fix crash upon logout at Restaurant page

--- react/src/services/user.service.js
+++ react/src/services/user.service.js
@@ -4,26 +4,6 @@ import authHeader from './auth-header';
 const API_URL = 'http://localhost:3000/';
 
 class UserService {
-    getPublicContent() {
-        return axios.get(API_URL + 'all');
-    }
-
-    getVendorBoard() {
-        return axios.get(API_URL + 'vendor', {headers: authHeader()});
-    }
-
-    getAdminBoard() {
-        return axios.get(API_URL + 'admin', {headers: authHeader()});
-    }
-
-    getTestBoard() {
-        return true;
-    }
-
-    getCustomer() {
-        return axios.get(API_URL + 'orderHistory', {headers: authHeader()});
-    }
-
     getAllRestaurants() {
         return axios.get(API_URL + 'api/vendor/restaurants', {headers: authHeader()});
     }

47b7f26f2ef0e95cd049e8c3791118f0d138ab36
Gerland Lok <gerlandl@sfu.ca>
2020-08-09 14:21:17 -0700

    Fixed issue where Empty Order History message did not appear when history is empty.

--- react/src/pages/OrderHistory.js
+++ react/src/pages/OrderHistory.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import UserService from "../services/user.service";
-import ReviewService from "../services/review.service";
 import AuthService from "../services/auth.service";
 import Orders from "../components/Order";
 import { withRouter } from "react-router";
@@ -9,7 +8,6 @@ import { CardColumns  } from "react-bootstrap";
 class OrderHistory extends React.Component {
     constructor(props) {
         super(props);
-        this.submitReview = this.submitReview.bind(this);
         this.state = {
             show: false,
             rating: 3,
@@ -54,49 +52,15 @@ class OrderHistory extends React.Component {
         )
     }
 
-    handleModal(item) {
-        this.setState({ show: !this.state.show })
-        // Clear changes to ratings.
-        this.setState({ restaurant_id: item.restaurant_id })
-        console.log(this.state.restaurant_id)
-        this.setState({ rating: 0 })
-        this.setState({ comment: '' })
-    }
-
-    closeModal() {
-        this.setState({ show: !this.state.show })
-        this.setState({ rating: 0 })
-        this.setState({ comment: '' })
-    }
-
-    changeRating = (newRating, name) => {
-        this.setState({ rating: newRating });
-    }
-    onChangeComment = (e) => {
-        this.setState({
-            comment: e.target.value
-        });
-    }
-
-    submitReview(item) {
-        ReviewService.addReview(this.state.restaurant_id, this.state.rating, this.state.comment).then(
-            response => {
-                console.log(response);
-            },
-            error => {
-                console.log(error);
-            }
-        );
-        alert("Thank you for sharing your experience!")
-        this.closeModal(item);
-    }
-
     render() {
         const orders = this.state.orders;
+        console.log(this.state.orders);
+        console.log(orders);
+        console.log(orders.length);
         return (
             <div>
                 <h1 className="h2  ml-4 my-5 green">Your Previous Orders</h1>
-                {orders ? (
+                {orders.length > 0 ? (
                     <div className="container mt-lg-5">
                         <CardColumns>
                             {this.state.orders.map((item, index) => (

1a7f9651dfdc1c169c35c77a72d701cf4e7ac9c1
Gerland Lok <gerlandl@sfu.ca>
2020-08-10 22:08:25 -0700

    Fixed delivery fee to display to 2 decimal places

--- react/src/pages/Restaurant.js
+++ react/src/pages/Restaurant.js
@@ -66,7 +66,7 @@ class Restaurant extends React.Component {
                     <Jumbotron>
                         <h1>{this.props.restaurant.name}</h1>
                         <p>{this.props.restaurant.address}</p>
-                        <p>Delivery fee: ${this.props.restaurant.deliveryfee}</p>
+                        <p>Delivery fee: ${this.props.restaurant.deliveryfee.toFixed(2)}</p>
                         <br></br>
                         {reviews.length > 0 ? (
                             <p>Reviews: {this.state.review[0].comments}</p>
